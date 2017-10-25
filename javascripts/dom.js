ad7de7f986b6a924a397ebe0fcea046cea8e34df
Gina Antonini <ginamantonini@gmail.com>
2017-10-25 18:46:46 -0500

    got current printing

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -3,27 +3,37 @@
 const outputDiv = $('#weatherHolder');
 
 
-const createCurrentDomString = (weatherArray) => {
+const createCurrentDomString = (weatherArray, days) => {
 	var currentString = "";
-		for (let i = 0; i < weatherArray.length; i++);
+	console.log("weatherArray", weatherArray);
+		currentString += 	`<h2>City: ${weatherArray.city.name}<h2>`;
+
+		for (let i = 0; i < days; i++) {
+			// if (i === 0) {
 		currentString += `<div>`;
-		currentString += 	`<h1>City: ${weatherArray[i].city.name}>`;
-		currentString += 	`<h2>Temperature: ${weatherArray[i].main.temp}</h1>`;
-		currentString += 	`<h2>Conditions: ${weatherArray[i].main.description}</h4>`;
-		currentString += 	`<h2>Air Pressure: ${weatherArray[i].main.pressure}</h4>`;
-		currentString += 	`<h2>Wind Speed: ${weatherArray[i].wind.speed}</h4>`;
-		currentString +=	`<button type="submit" id="threeDayButton">3 Day Forecast</button>`;
- 		currentString +=	`<button type="submit" id="sevenDayButton">7 Day Forecast</button>`;
+		currentString += 	`<h2>Temperature: ${weatherArray.list[i].main.temp}</h1>`;
+		currentString += 	`<h2>Conditions: ${weatherArray.list[i].weather[0].description}</h4>`;
+		currentString += 	`<h2>Air Pressure: ${weatherArray.list[i].main.pressure}</h4>`;
+		currentString += 	`<h2>Wind Speed: ${weatherArray.list[i].wind.speed}</h4>`;
 		currentString += `</div>`;
 		currentString += `</div>`;
 	printCurrentToDom(currentString);
+	// }
+	}	
 };
 
 const printCurrentToDom = (strang) => {
 	outputDiv.append(strang);
 };
 
-module.exports = createCurrentDomString;
+// const createForecastDomString = () => {
+// 	var forecastString = "";
+// 		for (let i = 0; i < weatherArray.length; i++);
+// 			forecastString += `<div>`;
+// 			forecastString
+// }
+
+module.exports = {createCurrentDomString};
 
 // Temperature
 // Conditions
